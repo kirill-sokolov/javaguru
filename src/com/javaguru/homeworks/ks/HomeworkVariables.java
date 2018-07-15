85219d739d7de2f687092f466070f37ba720b688
Kirill <kirill.sokolov.lv@gmail.com>
2018-07-15 15:58:19 +0300

    add 2 lesson homeworks - variable naming and types

---
+++ src/com/javaguru/homeworks/ks/HomeworkVariables.java
@@ -0,0 +1,68 @@
+package com.javaguru.homeworks.ks;
+
+class HomeworkVariables {
+
+    //1. количество кораблей в порту -
+    int shipCount;
+
+    //2. счётчик целых чисел -
+    int numberCounter;
+
+    //3. первое число -
+    int firstNumber;
+
+    //4. ответ на вопрос -
+    String answer;
+
+    //5. случайное число -
+    int randomIntNumber;
+
+    //5. случайное вещественное число -
+    double randomFloatNumber;
+
+    //6. генератор случайных чисел -
+    RandomNumberGenerator randomNumberGenerator;
+
+    //7. цвет автомобиля -
+    String carColor;
+
+    //8. скорость движения автомобиля -
+    double carSpeed;
+
+    //9. введённое пользователем целое число -
+    int userNumber;
+
+    //10. наибольшее число -
+    long maxIntNumber;
+
+    //11. наименьшее число -
+    long minIntNumber;
+
+    //11. наименьшее вещественное число -
+    double minFloatNumber;
+
+    //12. левая граница диапазона чисел -
+    long leftNumberBorder;
+
+    //13. правая граница диапазона чисел -
+    long rightNumberBorder;
+
+    //14. загаданное пользователем число -
+    int userAnswerNumber;
+
+    //15. вариант ответа -
+    String answerOption;
+
+    //16. банковский аккаунт - bankAccaunt
+    String bankAccaunt;
+
+    //17. количество денег на счёте -
+    double bankAccountMoneyAmmount;
+
+    //18. банковская карточка -
+    String bankCard;
+
+    //19. количество колёс у велосипеда -
+    byte bikeWheelCount;
+
+}
